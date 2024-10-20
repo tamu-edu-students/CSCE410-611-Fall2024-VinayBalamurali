1d61162c3298ba33b6b3c769a3e95600a18d4f62
Vinay Balamurali <vinay_b@tamu.edu>
2024-10-20 11:43:47 -0500

    * Completed Dev. Yet to test.

--- vm_pool.C
+++ vm_pool.C
@@ -46,23 +46,123 @@
 VMPool::VMPool(unsigned long  _base_address,
                unsigned long  _size,
                ContFramePool *_frame_pool,
-               PageTable     *_page_table) {
-    assert(false);
+               PageTable     *_page_table)
+{
+    baseAddress = _base_address;
+    size = _size;
+    availableMemory = size;
+    framePool = _frame_pool;
+    pageTable = _page_table;
+
+    next = nullptr;
+    totalRegions = 0;
+
+    pageTable->register_pool(this);
+
+    vmRegionInfo *region = (vmRegionInfo *)baseAddress;
+    region[0].baseAddress = baseAddress;
+    region[0].length = PageTable::PAGE_SIZE;
+    allRegions = region;
+
+    totalRegions = totalRegions + 1;
+
+    availableMemory = availableMemory - PageTable::PAGE_SIZE;
+
     Console::puts("Constructed VMPool object.\n");
-}
 
-unsigned long VMPool::allocate(unsigned long _size) {
-    assert(false);
+} // VMPool::VMPool
+
+
+unsigned long VMPool::allocate(unsigned long _size)
+{
+    if (_size > availableMemory)
+    {
+        Console::puts("Requested Memory greater than available size!\n");
+        return 0;
+    }
+
+    unsigned long pagesRequired = 0;
+
+    // Divide to get total pages.
+    pagesRequired = (_size / PageTable::PAGE_SIZE);
+
+    // We also need to account for the last page. We allocate an entire page
+    // and don't care about internal fragmentation.
+    unsigned long extraPages = (_size % PageTable::PAGE_SIZE) > 0 ? 1 : 0;
+    pagesRequired = pagesRequired + extraPages;
+
+    unsigned long requiredMemory = pagesRequired * PageTable::PAGE_SIZE;
+    unsigned long newBaseAddress = allRegions[totalRegions - 1].baseAddress + allRegions[totalRegions - 1].length;;
+    // totalRegions indexes the region that is being allocated.
+    allRegions[totalRegions].baseAddress = newBaseAddress;
+    allRegions[totalRegions].length = requiredMemory;
+
+    availableMemory = availableMemory - requiredMemory;
+
+    totalRegions = totalRegions + 1;
+
     Console::puts("Allocated region of memory.\n");
-}
 
-void VMPool::release(unsigned long _start_address) {
-    assert(false);
+    return newBaseAddress;
+
+} // VMPool::allocate
+
+
+void VMPool::release(unsigned long _start_address)
+{
+    int indexRegion = -1;
+
+    for (int i = 1 ; i < totalRegions; i++)
+    {
+        if (allRegions[i].baseAddress == _start_address)
+        {
+            indexRegion = i;
+        }
+    }
+
+    if (indexRegion == -1)
+    {
+        Console::puts("Given start address not found in the current VM pool!\n");
+        assert(false);
+    }
+
+    unsigned long pagesToRelease = (allRegions[indexRegion].length / PageTable::PAGE_SIZE);
+
+    unsigned long tempAddress = _start_address;
+    while (pagesToRelease > 0)
+    {
+        pageTable->free_page(tempAddress);
+
+        pagesToRelease = pagesToRelease - 1;
+
+        tempAddress = tempAddress + PageTable::PAGE_SIZE;
+    }
+
+    availableMemory =  availableMemory + allRegions[indexRegion].length;
+
+    // Shift all regions greater than the one to be freed
+    // to delete the current region.
+    for (int i = indexRegion; i < totalRegions; i++)
+    {
+        allRegions[i] = allRegions[i+1];
+    }
+
+    totalRegions = totalRegions - 1;
+
     Console::puts("Released region of memory.\n");
-}
 
-bool VMPool::is_legitimate(unsigned long _address) {
-    assert(false);
+} // VMPool::release
+
+
+bool VMPool::is_legitimate(unsigned long _address)
+{
     Console::puts("Checked whether address is part of an allocated region.\n");
-}
+    
+    if ((_address < baseAddress) || (_address > (baseAddress + size)))
+    {
+        return false;
+    }
 
+    return true;
+
+} // VMPool::is_legitimate
