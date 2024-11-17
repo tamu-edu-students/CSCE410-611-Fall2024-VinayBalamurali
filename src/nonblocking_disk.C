23beb48bdb9a96f70949564fe5cfef6f8d70b0fc
Vinay Balamurali <vinay_b@tamu.edu>
2024-11-17 16:30:24 -0600

    * Basic implementation of Locks.

--- src/nonblocking_disk.C
+++ src/nonblocking_disk.C
@@ -27,6 +27,9 @@
 
 extern Scheduler *SYSTEM_SCHEDULER;
 
+// We are limiting this system to have a maxium of 1000 concurrent threads
+const int maxThreads = 1000;
+
 /*--------------------------------------------------------------------------*/
 /* CONSTRUCTOR */
 /*--------------------------------------------------------------------------*/
@@ -34,11 +37,89 @@ extern Scheduler *SYSTEM_SCHEDULER;
 NonBlockingDisk::NonBlockingDisk(DISK_ID _disk_id, unsigned int _size) 
   : SimpleDisk(_disk_id, _size)
 {
+    level = new int[maxThreads];
+    for (int i = 0; i < maxThreads; i++)
+    {
+        level[i] = -1;
+    }
+
+    victim = new int [maxThreads - 1];
     ioBlockedQueue = new schedulerQueue();
 
     Console::puts("Constructed object of non blocking disk.\n");
+
 } // NonBlockingDisk::NonBlockingDisk
 
+/*--------------------------------------------------------------------------*/
+/* ADDITIONAL FUNCTIONS */
+/*--------------------------------------------------------------------------*/
+
+bool NonBlockingDisk::checkIfEqualOrGreater(int currentThread, int index)
+{
+    for (int i = 0; i < maxThreads; i++)
+    {
+        if ((i != currentThread) && (level[i] >= index))
+        {
+            return true;
+        }
+    }
+
+    return false;
+
+} // NonBlockingDisk::checkIfEqualOrGreater
+
+
+void NonBlockingDisk::acquireLock(void)
+{
+    int threadID = Thread::CurrentThread()->ThreadId();
+    for (int i = 0; i < (maxThreads - 1); i++)
+    {
+        level[threadID] = i;
+        victim[i] = threadID;
+
+        while (checkIfEqualOrGreater(threadID, i) &&
+               (victim[i] == threadID));
+    }
+
+    Console::puts("Lock acquired\n");
+
+} // NonBlockingDisk::acquireLock
+
+
+void NonBlockingDisk::releaseLock(void)
+{
+    int threadID = Thread::CurrentThread()->ThreadId();
+    level[threadID] = -1;
+
+    Console::puts("Lock released\n");
+
+} // NonBlockingDisk::releaseLock
+
+
+bool NonBlockingDisk::isThreadReady(void)
+{
+    if ((is_ready()) && (ioBlockedQueue->fetchSize() > 0))
+    {
+        return true;
+    }
+
+    return false;
+
+} // NonBlockingDisk::isThreadReady
+
+
+Thread* NonBlockingDisk::scheduleBlockedThread(void)
+{
+    if (ioBlockedQueue->fetchSize() >  0)
+    {
+        Console::puts("Fetching from Blocked queue\n");
+        Thread *popThread = ioBlockedQueue->dequeue();
+        return popThread;
+    }
+
+    return nullptr;
+
+} // NonBlockingDisk::scheduleBlockedThread
 
 /*--------------------------------------------------------------------------*/
 /* SIMPLE_DISK FUNCTIONS */
@@ -62,64 +143,44 @@ void NonBlockingDisk::wait_until_ready(void)
 } // NonBlockingDisk::wait_until_ready
 
 
-bool NonBlockingDisk::isThreadReady(void)
-{
-    if ((is_ready()) && (ioBlockedQueue->fetchSize() > 0))
-    {
-        // Console::puts("Thread is ready.\n");
-        return true;
-    }
-
-    // Console::puts("Thread is not ready\n");
-    return false;
-
-} // NonBlockingDisk::isThreadReady
-
-
-Thread* NonBlockingDisk::scheduleBlockedThread(void)
-{
-    if (ioBlockedQueue->fetchSize() >  0)
-    {
-        Console::puts("Fetching from Blocked queue\n");
-        Thread *popThread = ioBlockedQueue->dequeue();
-        return popThread;
-    }
-
-    return nullptr;
-
-} // NonBlockingDisk::scheduleBlockedThread
-
-
 void NonBlockingDisk::read(unsigned long _block_no, unsigned char * _buf)
 {
+    acquireLock();
     issue_operation(DISK_OPERATION::READ, _block_no);
+    releaseLock();
 
     wait_until_ready();
 
     /* read data from port */
-	int i;
-	unsigned short tmpw;
-	for (i = 0; i < 256; i++) {
-		tmpw = Machine::inportw(0x1F0);
-		_buf[i * 2] = (unsigned char)tmpw;
-		_buf[i * 2 + 1] = (unsigned char)(tmpw >> 8);
-	}
+    acquireLock();
+    int i;
+    unsigned short tmpw;
+    for (i = 0; i < 256; i++) {
+        tmpw = Machine::inportw(0x1F0);
+        _buf[i * 2] = (unsigned char)tmpw;
+        _buf[i * 2 + 1] = (unsigned char)(tmpw >> 8);
+    }
+    releaseLock();
 
 } // NonBlockingDisk::read
 
 
 void NonBlockingDisk::write(unsigned long _block_no, unsigned char * _buf)
 {
+    acquireLock();
     issue_operation(DISK_OPERATION::WRITE, _block_no);
+    releaseLock();
 
-	wait_until_ready();
+    wait_until_ready();
 
-	/* write data to port */
-	int i;
-	unsigned short tmpw;
-	for (i = 0; i < 256; i++) {
-		tmpw = _buf[2 * i] | (_buf[2 * i + 1] << 8);
-		Machine::outportw(0x1F0, tmpw);
-	}
+    /* write data to port */
+    acquireLock();
+    int i;
+    unsigned short tmpw;
+    for (i = 0; i < 256; i++) {
+        tmpw = _buf[2 * i] | (_buf[2 * i + 1] << 8);
+        Machine::outportw(0x1F0, tmpw);
+    }
+    releaseLock();
 
 } // NonBlockingDisk::write
